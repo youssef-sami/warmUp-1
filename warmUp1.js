d54e10c758a482befd8431258478620c22d73772
YaraSalhi <your_email@whatever.comyara.salhi@outlook.sagit config --global core.autocrlf inputgit config --global core.safecrlf truegit config --global user.email your_email@whatever.comyara.salhi@outlook.sa>
2019-08-25 10:18:05 +0300

    “warmUp1”

--- warmUp1.js
+++ warmUp1.js
@@ -4,3 +4,16 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+"Nahla" + " " + "Masri";
+
+13%3 === 0;
+function sum (array){
+	var result=0;
+	for(var i =0;i<=array.length,i++){
+		result += array[i];
+	}
+	return result
+}
+(13+14+13+15+16+17+19+13+16+15)/10;
+30*365*24*60*60;
+
