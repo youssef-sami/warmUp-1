3ba9dbe6702779578319ab37ea3afd8cfa53a96e
nuhla <nuhlamasri@gmail.com>
2019-08-26 10:46:46 +0300

    our secound day 2

--- warmUp2.js
+++ warmUp2.js
@@ -1,13 +1,28 @@
 // 1-write a function that returns the product of any number by 3.
+	function myltiplyby(x){
+		return x*3;
+	}
 // 2-complete the following function multiply to return the result of multiplying of x and y
 	function multiply(x,y){
-		//your code here
+		return x*y;
 	}
 
 // 3-complete the following function that Convert Fahrenheit to Celsius if the module for converting is 
 // 	(5/9) * (fahrenheit-32);
 	function toCelsius(fahrenheit){
+		return(5/9)* (fahrenheit-32);
 		//your code is here
 	}
 
 // 4-Define a function named "sqArea", and make it display square area if you knew the length of its side. 
+	function sqArea(length)
+	{
+		var str ="";
+		for(var i= 0 ; i<length ; i++){
+			for(var j= 0 ; j<length ; j++){
+			str+="*"
+		}
+		str +="\n";
+			}
+			return str;
+	}
