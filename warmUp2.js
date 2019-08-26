2ebf86c9883514ea9ba11fa78a8dfd152030de85
AlaaSarhan9 <alaa8917@hotmail.com>
2019-08-26 18:23:14 +0300

    new chang

--- warmUp2.js
+++ warmUp2.js
@@ -8,14 +8,14 @@ function multiplyByThree(num){
 		return x*y;
 	}
 
-// 3-complete the following function that Convert Fahrenheit to Celsius if the module for converting is 
+// /3-complete the following function that Convert Fahrenheit to Celsius if the module for converting is 
 // 	(5/9) * (fahrenheit-32);
 	function toCelsius(fahrenheit){
 		return (5/9)*(fahrenheit-32);
 		//your code is here
 	}
 
-// 4-Define a function named "sqArea", and make it display square area if you knew the length of its side. 
+/// 4-Define a function named "sqArea", and make it display square area if you knew the length of its side. 
 function sqArea (length){
 	area = Math.pow(length,2);
 	return Math.pow(area,2);
