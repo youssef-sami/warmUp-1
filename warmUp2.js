8b28fb645b09908673009efaa82662136253a42b
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2019-08-27 09:24:28 +0300

    fix

--- warmUp2.js
+++ warmUp2.js
@@ -1,20 +1,13 @@
 // 1-write a function that returns the product of any number by 3.
-function multiplyBy3(number) {
-  return number * 3;
-}
 // 2-complete the following function multiply to return the result of multiplying of x and y
-function multiply(x, y) {
-  return x * y;
-}
+	function multiply(x,y){
+		//your code here
+	}
 
-// 3-complete the following function that Convert Fahrenheit to Celsius if the module for converting is
+// 3-complete the following function that Convert Fahrenheit to Celsius if the module for converting is 
 // 	(5/9) * (fahrenheit-32);
-function toCelsius(fahrenheit) {
-  return (5 / 9) * (fahrenheit - 32);
-}
+	function toCelsius(fahrenheit){
+		//your code is here
+	}
 
-// 4-Define a function named "sqArea", and make it display square area if you knew the length of its side.
-function sqArea(length) {
-  //Square area formula equals side times side
-  return length * length;
-}
+// 4-Define a function named "sqArea", and make it display square area if you knew the length of its side. 
