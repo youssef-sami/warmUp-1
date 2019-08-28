7ad7ba472d23f8779788241266bbf7fb6b4c9dff
YasmeenOthman <yasmeen.othman20@gmail.com>
2019-08-28 14:40:57 +0300

    WarmUp4

--- warmUp4.js
+++ warmUp4.js
@@ -1,8 +1,6 @@
 // 1-Choose the correct comparison operator to display "true", when: 5 is less than 7.
-var isLess = (5 < 7);
 // 2-Use a conditional operator in which If the variable age is a value below 18 and the variable type is "male", 
 //the value of the variable voteable should be "Too young", otherwise the value of voteable should be "Old enough".
-<<<<<<< HEAD
 
 //Q1
 return (5 < 7);
@@ -17,17 +15,4 @@ if (voteable < 18){
 	return "Too young";
 }else{
 	return "Old enough";
-=======
-var age  = 15 ;
-var type = "male";
-var voteable = "";
-
-if(age < 18 && type === " male"){
-
-	voteable = "Too young";
-
-}else {
-	
-	voteable = "Old enough";
->>>>>>> 8de666ac90a6222cb88950a42de1a3971a85fff3
 }
