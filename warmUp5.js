c1326eb7129c75e311a9cfc4910a04bbcc9e7fc8
FidaaYousef <fidaa.as@gmail.com>
2019-08-29 09:53:50 +0300

    warmUp5

--- warmUp5.js
+++ warmUp5.js
@@ -4,3 +4,59 @@
 // eg=['hi','hello','welcome','hy'] == > ["hi","hy"]
 // 3-write a function that take array if numbers and returns array of all the prime numbers in that array.
 // prime([29,19,1,17,3,13,10]) ==> [29,19,17,13]
+
+
+1- 
+function reversString (string){
+	var i = 0;
+	var str ;
+
+	while (i < string [i]){
+		return str.puch(string(i)) + i ;
+		string.slice(i);
+
+	}
+	return str;
+	
+}
+
+
+2- 
+
+function each (array,func){
+	for (var i=0; i< array.length; i++)
+		func([i])
+}
+
+function sameLength(string){
+	var newArray = [];
+	var min = string[0].length;
+	 each(string,function(value){
+	 	if(value.length === min){
+	 		return newArray.push(value);
+	 	}
+	 })
+	 return newArray;
+	}
+
+
+
+3-
+	function primeNumber(array){
+		var newArray = [];
+
+		each(array,function(value){
+			if(value % 2 !==0  || value % 5 !== 0 || value % 7 !==0  || value % 11 !==0   || value % 2 !==13 ){
+
+				return newArray.push(value);
+			}
+		})
+
+
+		return newArray;
+
+	}
+
+
+
+
