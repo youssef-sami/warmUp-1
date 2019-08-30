1ebb0b1febe95e8c828f014c711ba9b25747a2f4
aajour <ahmed.ajourr@gmail.com>
2019-08-30 16:53:12 +0300

    warmUp5 final solution and refactor

--- warmUp5.js
+++ warmUp5.js
@@ -20,22 +20,44 @@ function reverseWithNumber(str) {
 // eg=['hi','hello','welcome','hy'] == > ["hi","hy"]
 
 
-function sameLength (arr) {
-	var same=[];
-	for (var i = 0; i < arr.length; i++) {
-		for (var j = 0; j >= i; j++) {
-			if(arr[i].length === arr[j].length) {
-				same.push(arr[i])
-				same.push(arr[j])
-
+function sameLength (arr){
+	//Define the result array
+	var result=[];
+	//this loop will take every string
+    for (var i = 0; i < arr.length; i++) {
+    	//and this one will compare it with the rest texts
+		  for (var j = i+1; j < arr.length ; j++) {
+			  if(arr[i].length === arr[j].length){
+				  result.push(arr[i],arr[j]);
 			}
 		}
 	}
-	return same;
+			return result;
 }
 
 
 // 3-write a function that take array if numbers and returns array of all the prime numbers in that array.
 // prime([29,19,1,17,3,13,10]) ==> [29,19,17,13]
 
+//First i'll use a function that checks if a number is prime or not
+function isPrime(number) {
+    //if the number % all of the numbers befor it with reminder then it's a prime 
+    for (var i = 2; i < number; i++)
+        if (number % i === 0) {
+            return false
+        }
+    // return the number if it's not 1
+    return number > 1;
+}
 
+//then i'll use the above function on our passed array
+function getAllPrrimes(arr) {
+    var primes = [];
+    for (var i = 0; i <= arr.length; i++) {
+        // push to the result (primes) array
+        if (isPrime(arr[i])) {
+            primes.push(arr[i])
+        }
+    }
+    return primes;
+}
