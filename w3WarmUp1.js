c4b87b82317e3a9d916d0286786bbbe4e789c552
aajour <ahmed.ajourr@gmail.com>
2019-09-01 09:15:48 +0300

    w3WarmUp1 solution

--- w3WarmUp1.js
+++ w3WarmUp1.js
@@ -1,5 +1,10 @@
 // 1-Create a third variable called z, assign x + y to it, and display it.
 	var x = 5;
 	var y = 10;
+	var z = x + y;
+	console.log(z)
 // 2-calculate the area of a square using varaiables.
-
+function areaOfSquare (side) {
+	//the area of a square equals double of the entered side
+	return 'area of the square equales '+ side*side;
+}
