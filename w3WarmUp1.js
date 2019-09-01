8a7bbc0e60720c0304824b9bb959344247d1c6b0
Tasneem Al Haj <zakarneh.tasneem@gmail.com>
2019-09-01 09:15:34 +0300

    w3WarmUp

--- w3WarmUp1.js
+++ w3WarmUp1.js
@@ -1,5 +1,12 @@
 // 1-Create a third variable called z, assign x + y to it, and display it.
 	var x = 5;
 	var y = 10;
+	var z = x+y;
+	console.log(z);
+
 // 2-calculate the area of a square using varaiables.
+var length= 1;
+var area = length*length;
+consol.log(area);
+
 
