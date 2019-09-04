2cc9c749588608ead916fca774fbaab02f22d061
AlaaSarhan9 <alaa8917@hotmail.com>
2019-09-04 09:42:51 +0300

    function

--- w3WarmUp4.js
+++ w3WarmUp4.js
@@ -1,5 +1,35 @@
 // 1-Write a function that takes a string as an input and returns the reverse of each letter 
 //followed by a number starting from zero(solve it using while loop).
 //     reverseStr('hello'); "o1l2l3e4h"
+
+
+
+function reverseStr(string){
+	var i=1;
+	while(i< string.length){
+		string.split("")
+	string[0]= string[string.length];			  
+		return string+i;
+	 i++;
+	}
+}
+
+
+
+
 // 2-write a function that takes array of strings and returns an array of the strings that have the same length
 // eg=['hi','hello','welcome','hy'] == > ["hi","hy"]
+
+
+function sameLength(arrayOfStr){
+	newArr=[];
+for (var i=0; i<arrayOfStr.length ; i++){
+	if (arrayOfStr[0].length===arrayOfStr[i].length){
+		newArr.push (arrayOfStr[i]);
+	}
+}
+	return newArr;
+
+}
+
+
