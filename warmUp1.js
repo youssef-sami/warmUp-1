95b83098dd207f08a991a35af2d6e4b43b5b7615
abudiab <mahmoud.w.diab.com>
2019-09-04 21:41:09 +0300

    M.Diab WarmUp1

--- warmUp1.js
+++ warmUp1.js
@@ -5,34 +5,41 @@
 
 // your code is here
 
-// Q1
-console.log("Yara" + "Salhi");
+var fullName = "Mahmoud" + " Diab";
+console.log(fullName);
 
-//Q2
-function isMultipleOf13(num) {
-  //We know the number is multiple of another number if the reminder =0
-  //so we need to use the Mod opertator
-  if (num % 13 == 0) {
-    return num + "  is a multiple of 13";
-  } else {
-    return num + "  is not a multiple of 13";
-  }
-}
+----
 
-//Q3
-function getAvg(arr) {
-  //define a total varible that will contain all numbers
+// function isMultipleOf3(number) {
+// 	if (number%3 === 0) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+// isMultipleOf3(13); //false
+
+13%3 === 0 ? console.log('Yes') : console.log('No');
+
+----
+function findAverage(array) {
   var total = 0;
-  for (var i = 0; i < arr.length; i++) {
-    //add every array element to the total
-    total += arr[i];
+  for (var i = 0; i < array.length; i++) {
+    total += array[i];
   }
-  console.log(total);
-  return total / arr.length;
-}
+  var average = total/ array.length;
+  return average;
+};
 
-//Q4
-function yearsToSeconds(years) {
-  //Years*months*days*hours*minutes*seconds
-  return years * 12 * 30 * 24 * 60 * 60;
-}
+var ages = [13,14,13,15,16,17,19,13,16,15];
+findAverage(ages); //15.1
+
+----
+function calculateAgeInSeconds (age) {
+  var ageInSeconds = age * 31557600;
+  return ageInSeconds;
+};
+
+calculateAgeInSeconds(25); //788940000
+
+---
