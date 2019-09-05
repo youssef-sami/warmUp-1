f6b9270e23f54b8579c2cea672d6b4e6969a1447
YasmeenOthman <yasmeen.othman20@gmail.com>
2019-09-05 17:32:27 +0300

    W3warmUp5

--- w3Warmup5.js
+++ w3Warmup5.js
@@ -7,3 +7,48 @@
 //     -calculate the number of male friends that your class have by writing a function called nbOfMale.
 //     -Write a function searchMates that, given a query and an array of Mates,
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+function classMates (name,age,specialization){
+	retrn {
+		name : name,
+		age : age,
+		specialization : specialization,
+		gender :gender
+	}
+}
+
+var classmate1 = classMates("Mays", 19, "CS", "female");
+var classmate2 = classMates( "Basima", 24, "CS","female");
+var classmate3 = classMates( "Alla", 30, "Industrial Engineer", "female");
+var classmate4 = classMates ( "Ahmed", 24, "CS","male");
+var classMatesArray = [classmate1, classmate2, classmate3];
+
+function displayFriend (obj){
+	return "My friend" + " " + obj.name + " whose now in her"+ " " + obj.age + " specilaized in" + obj.specialization;
+}
+
+function addFriend(classMatesArray,classmate){
+	classMatesArray.push(classmate);
+}
+function nbOfMale(array){
+var count = 0;
+for (var i = 0; i < array.length; i++) {
+	if(array[i].gender === "male"){
+		count ++ ;
+	}
+}
+return count;
+
+}
+
+
+function searchMates(classMatesArray,age){
+	var result = {};
+	for (var i = 0; i < classMatesArray.length; i++) {
+		if(classMatesArray[i].age === age){
+			result[i] = classMatesArray[i]
+		}
+	}
+	return result;
+}
+
+
