09b757222afa5c9da5ccdf7f80b83f31e5d7439c
mohammad annab <eng.mannab@yahoo.com>
2019-09-06 00:19:48 +0300

    warmup 5 is refactored and done

--- w3Warmup5.js
+++ w3Warmup5.js
@@ -10,18 +10,99 @@
 
 
 
+ 
+
+// Part a
+
 mame, age, major, gender
 
-function classMates(name, age, major, gender){
-	var claMat: {
-		name: 'name',
+// all of the classmates have these properties in common
+
+
+
+// Part b
+
+ function classMates(name, age, major, gender){
+	return  {
+		name: name,
 		age: age,
-		major: 'major',
-		gender: 'gender'
+		major: major,
+		gender: gender
 	}
 }
 
 
-var mate1 = classmates('ameer', 26, 'M.E', 'male');
+	var mate1 = classMates('ameer', 22, 'M.E', 'male');
+	var mate2 = classMates('naser', 27, 'A.E', 'male');
+	var mate3 = classMates('noor', 25, 'B.A', 'female');
+	var mate4 = classMates('nirmeen', 30, 'N.S', 'female');
 
-var classMates = []
+// this function is a factory function that creates objects assigned to each classmate with his/her propertirs or characteristics  
+// after creating the factory function i used it to create my classmates 
+
+
+
+// Part c
+
+var mates = [mate1, mate2, mate3, mate4];
+
+// this array contains all of my classmates that i created using the factory function
+
+
+
+
+// part d   
+
+function displayFriend(mate){
+	return 'This my classmate and friend' + ',' + ' ' + 'his/her name is' + ' ' + classMates.name + ',' + ' ' + 'his/her age is' + ' ' + classMates.age + ',' + ' ' + 'his/her major is' + ' ' + classMates.major + ' ' + 'and his/her gender is' + ' ' + classMates.gender + '.'; 
+}
+
+// this function takes a mate which is an object that was created by the factory function and displays the important characteristics of the mateas as string 
+
+
+
+
+
+// part e
+
+function addFriend(mate){
+
+	return mates.push(mate);
+}
+
+// this function takes a mate that was created using the factory function and pushes it into the array of the mates
+
+
+
+
+// part f 
+
+
+ function nbOfMale(array){
+ 	var males = 0;
+ 	for (var i = 0; i<array.length; i++){
+ 		if(array[i].gender = 'male'){
+ 			males ++;
+ 		}
+ 	}
+ 	return males;
+ }
+
+// this function takes an array of mates which are objects and iterates over the elements of the array and checks the gender in each element and if it is male it returns 
+// this element into a new array which is called males
+
+// part g
+
+function searchMates(query, array){
+	var info = [];
+	for (var i = 0; i<array.length; i++){
+			if(query === array[i].age){
+				info.push(array[i]);
+		}
+	}
+	return info;
+}
+
+// this function takes a query about the age and an array and then iterates over the elements of the array which are objects and sees if the
+ // age in this object matches the query and if so returns the object in a new array
+
