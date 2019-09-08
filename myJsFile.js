1dd175d6f91c860382cec5278320587f3dae294e
zinah aroqi <zihna-19@hotmail.com>
2019-09-08 10:59:48 +0300

    solving warm up 1 for week 4

---
+++ myJsFile.js
@@ -0,0 +1,22 @@
+function importantClicked() {
+
+	document.getElementsByTagName('body')[0].style.backgroundColor = "red";
+	document.getElementsByTagName('div')[0].style.color = "white";
+	document.getElementsByTagName('h3')[0].style.color = "darkrd";
+
+}
+
+function normalClicked() {
+	
+	document.getElementsByTagName('body')[0].style.backgroundColor = "lightblue";
+	document.getElementsByTagName('div')[0].style.color = "white";
+	document.getElementsByTagName('h3')[0].style.color = "darkblue";
+
+}
+
+function restClicked() {
+	
+	document.getElementsByTagName('body')[0].style.backgroundColor = "white";
+	document.getElementsByTagName('div')[0].style.color = "darkgrey";
+
+}
