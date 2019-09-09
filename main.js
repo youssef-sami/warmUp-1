b74b302f4b1a34b5d64246f6736a083c7fd0b07a
YasmeenOthman <yasmeen.othman20@gmail.com>
2019-09-09 09:43:32 +0300

    W4warmUp2

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// 	var num1 = document.getElementById('first');
+// 	var num2 = document.getElementById('third');
+// 	var opr = document.getElementById('second');
+// 	var result = document.getElementById('result');
+	
+
+
+function click (){
+	$('#btn').on('click',function(){
+		$('#first').
+	})
+}
