07b4d4c87d8062f5d05ebdd292d0cf56813b6379
YaraSalhi <yara.salhi@outlook.sa>
2019-09-09 23:56:38 -0700

    w4warmup3yara

--- w4WarmUp3.js
+++ w4WarmUp3.js
@@ -5,3 +5,11 @@
 // Using jQuery, create a function, that takes the input from the text field, creates a list item and add it to the unordered list, then add a class to the added item alternatively (So first time Class1 then next time Class2 then Class1 and repeat)
 
 // Good Luck!
+
+
+
+
+
+
+
+
