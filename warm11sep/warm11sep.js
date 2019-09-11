b32df31f84b642f4c69946b8c073d7001c528cbd
FidaaYousef <fidaa.as@gmail.com>
2019-09-11 10:28:01 +0300

    11sep

---
+++ warm11sep/warm11sep.js
@@ -0,0 +1,14 @@
+
+
+//Write a jQuery function that creates a new element in the left unordered list 
+//if the “left” button was pressed before the “create” button 
+//and to the right unordered list if the “right” button was pressed before the “create” button
+
+
+	function create(te1){
+		if($("document.getElementsByClassName(right)").click()){
+		
+		  document.createElement("te1").innerHTML("rightList");
+		}
+
+}
---
+++ warm11sep/wensday.js
@@ -0,0 +1,21 @@
+
+
+// Using jQuery, create a function, that takes the input from the text field, 
+//creates a list item and add it to the unordered list, 
+//then add a class to the added item alternatively 
+//(So first time Class1 then next time Class2 then Class1 and repeat)
+
+
+
+
+
+	
+var te1= document.getElementsByClassName('texts');
+
+
+
+	function operation(te1){
+		console.log(document.createElement("te1"))
+		 return document.createElement("te1").append("mylist");
+
+}
