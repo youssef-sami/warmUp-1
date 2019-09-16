15483876da74f0818846d4b7bc0301240af72029
nouranad <noranadel97@gamil.com>
2019-09-16 09:58:29 +0300

    warm2

---
+++ w5WarmUp2.js
@@ -0,0 +1,30 @@
+//Create a function (class) called Student that return an object that contains the following properties:
+//Name, Date of Birth, Gender, Age
+ function (student) {
+	return function ( details) {
+		var stuName = "name";
+		var stuDatrOfBirth = x ;
+		var stuGender = y;
+		var stuAge = num ;
+	}
+	return student ;
+}
+
+//Create an array and fill it with 5 students
+
+var students =["Ali","Noran","ola","Adel","Hadeel"]
+
+//Create a function that takes a student as parameter and print out the details of the student
+
+
+
+//Create a function that takes the array of students and sort them out based on age (younger-to-older)
+
+function youngerToOlder(students , age) {
+	return filter (students , function(student , num) {
+		return student.charAt(0) === age ;
+	})
+}
+
+
+//Create a function that takes the array and a string either “M” or “F” and returns the male students if “M” was passed and female students if “F” was passed
