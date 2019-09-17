720020bec83c8e2866bfb9884b1f3c89c7c91b2b
aqaddoumi <qaddoumi.adel@gmail.com>
2019-09-17 14:19:52 +0300

    Revert ""w5 warm up 3"

--- warmUp2.js
+++ warmUp2.js
@@ -1,26 +1,13 @@
 // 1-write a function that returns the product of any number by 3.
-
-function multiplyByThree(x){
-	return x*3;
-	}
-
 // 2-complete the following function multiply to return the result of multiplying of x and y
 	function multiply(x,y){
-		return x*y;
+		//your code here
 	}
 
 // 3-complete the following function that Convert Fahrenheit to Celsius if the module for converting is 
 // 	(5/9) * (fahrenheit-32);
 	function toCelsius(fahrenheit){
-		return (5/9) * (fahrenheit-32);;
+		//your code is here
 	}
 
-<<<<<<< HEAD
 // 4-Define a function named "sqArea", and make it display square area if you knew the length of its side. 
-function sqArea(side){
-		//your code here
-		return side*side;
-	}
-=======
-// 4-Define a function named "sqArea", and make it display square area if you knew the length of its side. 
->>>>>>> 9f4a4af5ede9b961d5eaa16c50053fc9d56c95dd
